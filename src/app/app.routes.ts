9790575fd200a99ae60dee922aaa0e7e3d8f3f26
Gisselle Jimenez <gissellej274@gmail.com>
2026-04-20 20:53:06 -0600

    Cambios antes de cambiar de rama

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { LaboralComponent } from './pages/laboral/laboral.component';
 import { PosgradoComponent } from './pages/posgrado/posgrado.component';
-import { CertificacionesComponent } from './pages/certificaciones/certificaciones.component'; // 👈 IMPORTANTE
+import { CertificacionesComponent } from './pages/certificaciones/certificaciones.component'; 
+
 
 export const routes: Routes = [
   { path: 'laboral', component: LaboralComponent },
